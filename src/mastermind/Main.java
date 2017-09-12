bee68dca4b732c8294fb5052341aea210cacb3c5
gruecke2 <31459637+gruecke2@users.noreply.github.com>
2017-09-12 16:58:04 -0600

    Update Main.java
    I don't wanna do the changes to everything else in window yet on our Github version, but I changed all instances of guessTurn to check.guessTurn and created objects like this:
    
    //create necessary logic objects
    	CodeMaster master = new CodeMaster();
    	Marble_Slot user = new Marble_Slot();
    	Feedback check = new Feedback();

--- src/mastermind/Main.java
+++ src/mastermind/Main.java
@@ -3,10 +3,31 @@ package mastermind;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Window window = new Window();
+		window.master.setRandomAnswer();
+		int turn = 0;
 		
+		
+		//this whole block is either running infinitely or not at all.
+		
+//		if(turn >= window.check.getGuessTurn());
+//		{
+//			do
+//			{
+//				//as of now, must do blackPegNum first
+//				window.check.blackPegNum(window.user.getGuess(), window.master.getAnswer());
+//				window.check.whitePegNum(window.user.getGuess(), window.master.getAnswer());
+//				
+//				for(Colors el : window.master.getAnswer()){
+//					System.out.println(el);
+//				}
+//				
+//				System.out.println("Black: " + window.check.getBlackCorrect());
+//				System.out.println("White: " + window.check.getWhiteCorrect());
+//				turn++;
+//			}while(window.check.getGuessTurn() < 9 && window.check.getBlackCorrect() != 4);
+//			
+//		}	
 	}
 
 }
