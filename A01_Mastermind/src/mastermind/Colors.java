86bdfe21b62b1126488349a7a78ab49de8bc2cf9
gruecke2 <31459637+gruecke2@users.noreply.github.com>
2017-09-16 23:16:26 -0600

    added comments to Colors

--- A01_Mastermind/src/mastermind/Colors.java
+++ A01_Mastermind/src/mastermind/Colors.java
@@ -11,16 +11,18 @@
  *  Description : (Narrative desciption, not code)
  *
  *    1) What is the purpose of the code; what problem does the code solve.
- *    		This is a game of Mastermind
+ *    		Enum Colors is a enumeration for the possible colors that a marble could display.
+ *	Blank is added at the end, but never included in the getRandom method's output. 
  *
  *    2) What data-structures are used.
- *    		Enum, Array, ActionListener, Swing GUI, List
+ *    		Enum, random method from Java Math
  *
  *    3) What algorithms, techniques, etc. are used in implementing the data structures.
- *    		For Loops, Switch Statements, List
+ *    		Java's Math.random, array length, casting
  *
  *    4) What methods are implemented (optional).
- *
+ *		Only method getRandom gets random values from BLUE, GREEN, RED, WHITE, YELLOW, BLACK as
+ *	possibilities. Used to generate our random code for the player to guess.
  *  Changes :  <Description|date of modifications>
  *
  ********************************************************/
